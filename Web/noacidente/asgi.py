3250ca2bc115d9d2bb58d7ac95ae2816d02ea428
Max <misterpollini@gmail.com>
2022-09-05 21:34:55 -0300

    Push de test
    Salude al manu en un comentario no importante

--- Web/noacidente/asgi.py
+++ Web/noacidente/asgi.py
@@ -3,7 +3,7 @@ ASGI config for noacidente project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see // ola manu
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
