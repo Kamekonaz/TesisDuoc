2f539a7df4f525f2f216279bba37900d545f234a
YoSoyCaty <je.devaud@duocuc.cl>
2022-10-02 20:11:45 -0300

    datos de prueba

--- Archivos SQL/Insertar tablas de prueba.sql
+++ Archivos SQL/Insertar tablas de prueba.sql
@@ -3,26 +3,129 @@ truncate table usuario;
 truncate table cuenta;
 truncate table tipo_usuario;
 
+-- Tipos de usuario: No tocar
 insert into tipo_usuario values(1, 'ADMINISTRADOR');
 insert into tipo_usuario values(2, 'PROFESIONAL');
 insert into tipo_usuario values(3, 'CLIENTE');
 
-insert into cuenta values(1, 'robert_mansen', 
-'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 1);
-insert into cuenta values(2, 'robert_mansen2', 
-'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 2);
-insert into cuenta values(3, 'robert_mansen3', 
-'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 2);
+
+-- CREAR CUENTAS
+-------ADMINISTRADORES--------------------------------------------------------------------------------------------------------------------------
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(4, 'camila_pena', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 1);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(5, 'maximiliano_flores', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 1);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(6, 'manuel_bravo', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 1);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(7, 'michael_jara', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 1);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(8, 'jean_devaud', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 1);
+
+-------PROFESIONALES----------------------------------------------------------------------------------------------------------------------------
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(9, 'jo_perez', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 2);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(10, 'ma_torres', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 2);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(11, 'fe_correa', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 2);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(12, 'lu_flores', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 2);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(13, 'ju_guerra', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 2);
+
+-------CLIENTES---------------------------------------------------------------------------------------------------------------------------------
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(14, 'ama_lorenz', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 3);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(15, 'fla_josefo', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 3);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(16, 'ari_dela', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 3);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(17, 'dal_revis', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 1, 3);
+
+insert into cuenta(id_cuenta, username, password, estado, id_tipo)
+values(18, 'liz_pizarra', 'pbkdf2:sha256:260000$yXUkfCr9aGZZJE8W$a02456628256cd096f1834e243fb7eba613283ef70360e92d9c098e8641bd600', 0, 3);
 
 
-insert into usuario values('11.123.321-1', 'Robert1 Christopher', 'Mansen Guzman', 'robtopher@gmail.com', 991233214, null, 1);
-insert into usuario values('11.123.321-2', 'Robert2 Christopher', 'Mansen Guzman', 'robtopher@gmail.com', 991233214, null, 2);
-insert into usuario values('11.123.321-3', 'Robert3 Christopher', 'Mansen Guzman', 'robtopher@gmail.com', 991233214, null, 3);
 
 
-insert into empresa values('71.123.321-1', 'Empresa test 1', 912344321, 'Los tests 1', '11.123.321-1');
-insert into empresa values('71.123.321-2', 'Empresa test 2', 912344322, 'Los tests 2', '11.123.321-2');
-insert into empresa values('71.123.321-3', 'Empresa test 3', 912344323, 'Los tests 3', '11.123.321-3');
+-- CREAR USUARIOS
+-------ADMINISTRADORES-------------------------------------------------------------------------------------
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('20.123.321-1', 'Camila Paz', 'Pena Pereira', 'cami@gmail.com', 991233214, null, 4);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta) 
+values('19.123.321-2', 'Maximiliano Antonio', 'Flores Guzman', 'maxi@gmail.com', 991233214, null, 5);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('19.999.321-k', 'Manuel Christopher', 'Bravo penaloza', 'manu@gmail.com', 991233214, null, 6);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('20.753.381-4', 'Michael Luis', 'Jara Torrez', 'mike@gmail.com', 991233214, null, 7);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta) 
+values('20.533.117-4', 'Jean Pierre', 'Devaud Devaud', 'jean@gmail.com', 991233214, null, 8);
+
+-------PROFESIONALES-------------------------------------------------------------------------------------
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('11.738.385-4', 'Jose Luis', 'Perez Correa', 'jopeco@gmail.com', 991233214, null, 9);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('15.554.224-k', 'Maria Andrea', 'Torres Diaz', 'matodi@gmail.com', 991233214, null, 10);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta) 
+values('16.274.875-2', 'Federico Rodrigo', 'Correa Gonzalez', 'fecogo@gmail.com', 991233214, null, 11);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('14.344.656-2', 'Luis Alberto', 'Flores Dias', 'luflodi@gmail.com', 991233214, null, 12);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('15.743.952-4', 'Juan Luis', 'Guerra Perez', 'juguepe@gmail.com', 991233214, null, 13);
+
+-------CLIENTES---------------------------------------------------------------------------------------
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta) 
+values('11.987.158-k', 'Amanda Ana', 'Lorenz Gonzalez', 'amalogo@gmail.com', 991233214, null, 14);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('19.852.563-4', 'Flavio Josefino', 'Gonzalez Torres', 'flagote@gmail.com', 991233214, null, 15);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('10.349.167-4', 'Ariana Paz', 'Dela Diaz', 'aridedi@gmail.com', 991233214, null, 16);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta) 
+values('14.843.164-6', 'Dalas Antonio', 'Revis Perez', 'darepe@gmail.com', 991233214, null, 17);
+
+insert into usuario(rut_usuario, nombres, apellidos, email, telefono, imagen, id_cuenta)
+values('12.811.735-8', 'Lizeth Andrea', 'Pizarra Garcia', 'lipiga@gmail.com', 991233214, null, 18);
+
+
+-- CREAR EMPRESAS
+insert into empresa(rut_empresa, razon_social, telefono, nombre, rut_usuario)
+values('71.352.444-2', 'Empresa test 4', 912344323, 'Cordillera', '11.987.158-k');
+
+insert into empresa(rut_empresa, razon_social, telefono, nombre, rut_usuario)
+values('72.996.584-7', 'Empresa test 5', 912344323, 'Maestra', '19.852.563-4');
+
+insert into empresa(rut_empresa, razon_social, telefono, nombre, rut_usuario)
+values('65.463.248-7', 'Empresa test 6', 912344323, 'Swan', '10.349.167-4');
+
+insert into empresa(rut_empresa, razon_social, telefono, nombre, rut_usuario)
+values('57.733.549-2', 'Empresa test 7', 912344323, 'Los del NorteChico', '14.843.164-6');
+
+insert into empresa(rut_empresa, razon_social, telefono, nombre, rut_usuario)
+values('39.853.241-5', 'Empresa test 8', 912344323, 'Constructora San Luis', '12.811.735-8');
 
 
 COMMIT WORK;
